3efffd9919cd31dd196b1f498c00cb07bd2b37e5
23235aa <23235aa@hvhs.school.nz>
2026-06-05 12:28:05 +1200

    game setup

---
+++ fb_io.js
@@ -0,0 +1,29 @@
+var GLOBAL_user;
+ var authenticationListener;
+ function fb_login(){
+     authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+ }
+   function fb_handleLogin(_user){
+     if(_user){
+         console.log("User is logged in")
+         GLOBAL_user = _user;
+         firebase.database().ref('/Highscores/game1/users/' + GLOBAL_user.uid).update(
+            {
+                name: GLOBAL_user.displayName
+            }
+         );
+
+         
+     } else{
+         console.log("User is NOT logged in - Starting the popup process")
+           fb_popupLogin();
+     }
+ }
+   function fb_popupLogin(){
+     var provider = new firebase.auth.GoogleAuthProvider();
+ 
+     firebase.auth().signInWithPopup(provider).then((result) => {
+         GLOBAL_user = result.user; //Save the user details object to a global variable
+         console.log("User has logged in")
+     });
+ }
