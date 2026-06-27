476753a8da4bc398b3d0adeb45e5ef25dbb6a2d7
23235aa <23235aa@hvhs.school.nz>
2026-06-27 18:11:51 +1200

    login flow fixed

--- fb_io.js
+++ fb_io.js
@@ -3,36 +3,58 @@ var authenticationListener; //This is a global varaible that stores the listner
 function fb_login() { //Ths function sets up a listner for the log in state of the user 
     authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
 }
-function fb_handleLogin(_user) { //This functions runs if the log in state of the user changes, for example if they are logged in or out
+async function fb_handleLogin(_user) { //This functions runs if the log in state of the user changes, for example if they are logged in or out
     if (_user) {
-        console.log("User is logged in "+_user.displayName)
+        console.log("User is logged in " + _user.displayName)
         GLOBAL_user = _user;
         firebase.database().ref('/Highscores/userInfo/' + GLOBAL_user.uid).update(
             {
                 name: GLOBAL_user.displayName //Sets the actual name of the user that they have for the google account, not their username
             });
-            //Checks if the button is on the HTML page through document.getElementById and if it is it runs the code
-        if (document.getElementById("loginButton")) {
-            document.getElementById("loginButton").style.display = "none"; //If the user has already logged into the site before the login button does not appear
+        //Checks if the button is on the HTML page through document.getElementById and if it is it runs the code
+        var snapshot = await firebase.database().ref('/Highscores/userInfo/' + GLOBAL_user.uid).once('value')
+        var userLogin = snapshot.val()
+        if (userLogin && userLogin.age && userLogin.username) { //
+            if (document.getElementById("loginButton")) {
+                document.getElementById("loginButton").style.display = "none"; //If the user has already logged into the site before the login button does not appear
+            }
+            if (document.getElementById("loginWelcome")) {
+                document.getElementById("loginWelcome").style.display = "none";
+            }
+            if (document.getElementById("gameForm")) {
+                document.getElementById("gameForm").style.display = "none"; //Hides the form if you are already logged in
+            }
+            if (document.getElementById("gameButton")) { //Shows you a game button that takes you straight to game page 
+                document.getElementById("gameButton").style.display = "block";
+            }
+            if (document.getElementById("welcomeBack")) { //If you are logged in it says welcome back
+                document.getElementById("welcomeBack").style.display = "block"
+            }
+            if (document.getElementById("register")) { //Doesn't show you register text 
+                document.getElementById("register").style.display = "none"
+            }
+            //We do this through style.display="none" which hides the login button
         }
-        if(document.getElementById("loginWelcome")){
-            document.getElementById("loginWelcome").style.display="none";
+        else {
+            if (document.getElementById("loginButton")) {
+                document.getElementById("loginButton").style.display = "block" //If the user is not logged in it displays the button
+            }
+            if (document.getElementById("loginWelcome")) {
+                document.getElementById("loginWelcome").style.display = "block"
+            }
+            if (document.getElementById("gameForm")) {
+                document.getElementById("gameForm").style.display = "block" //Shows you the form if you are not logged in
+            }
+            if (document.getElementById("gameButton")) {
+                document.getElementById("gameButton").style.display = "none" //Doesn't show you the game button if you are not logged in
+            }
+            if (document.getElementById("register")) {
+                document.getElementById("register").style.display = "block"
+            }
+            if (document.getElementById("welcomeBack")) {
+                document.getElementById("welcomeBack").style.display = "none"
+            }
         }
-        if(document.getElementById("gameForm")){
-            document.getElementById("gameForm").style.display="none"; //Hides the form if you are already logged in
-        }
-        if(document.getElementById("gameButton")){ //Shows you a game button that takes you straight to game page 
-            document.getElementById("gameButton").style.display="block";
-        }
-        if(document.getElementById("welcomeBack")){ //If you are logged in it says welcome back
-            document.getElementById("welcomeBack").style.display="block"
-        }
-        if(document.getElementById("register")){ //Doesn't show you register text 
-            document.getElementById("register").style.display="none"
-        }
-        //We do this through style.display="none" which hides the login button
-
-
 
     } else {
         console.log("User is NOT logged in - Starting the popup process")
@@ -40,20 +62,20 @@ function fb_handleLogin(_user) { //This functions runs if the log in state of th
         if (document.getElementById("loginButton")) {
             document.getElementById("loginButton").style.display = "block" //If the user is not logged in it displays the button
         }
-        if(document.getElementById("loginWelcome")){
-            document.getElementById("loginWelcome").style.display="block"
+        if (document.getElementById("loginWelcome")) {
+            document.getElementById("loginWelcome").style.display = "block"
         }
-        if(document.getElementById("gameForm")){
-            document.getElementById("gameForm").style.display="block" //Shows you the form if you are not logged in
+        if (document.getElementById("gameForm")) {
+            document.getElementById("gameForm").style.display = "block" //Shows you the form if you are not logged in
         }
-        if(document.getElementById("gameButton")){
-            document.getElementById("gameButton").style.display="none" //Doesn't show you the game button if you are not logged in
+        if (document.getElementById("gameButton")) {
+            document.getElementById("gameButton").style.display = "none" //Doesn't show you the game button if you are not logged in
         }
-        if(document.getElementById("register")){
-            document.getElementById("register").style.display="block"
+        if (document.getElementById("register")) {
+            document.getElementById("register").style.display = "block"
         }
-        if(document.getElementById("welcomeBack")){
-            document.getElementById("welcomeBack").style.display="none"
+        if (document.getElementById("welcomeBack")) {
+            document.getElementById("welcomeBack").style.display = "none"
         }
     }
     showName()
@@ -65,9 +87,9 @@ function fb_popupLogin() { //Actually runs the google log in
         GLOBAL_user = result.user; //Save the user details object to a global variable
         console.log("User has logged in")
     });
-}  
+}
 
-async function showName(){
+async function showName() {
     var snapshot = await firebase.database().ref('/Highscores/userInfo/' + GLOBAL_user.uid).once('value');
     var userDataLogin = snapshot.val()
     document.getElementById('userDetails').innerHTML += "Hello " + userDataLogin.username
